5360fd288ec8af79c12f863240002ff4ec228ede
LuisFcamo <luis1170da@gmail.com>
2024-03-24 22:09:46 -0300

    tarefa de aed 2 finalizada

--- AED 2 (Algoritmos e estruta de dados 2)/Tarefas/01/01.c
+++ AED 2 (Algoritmos e estruta de dados 2)/Tarefas/01/01.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <unistd.h>
+
 #include <stdbool.h>
+
 #include <string.h>
 
 #define MAXLinha 50
@@ -13,22 +17,24 @@ typedef struct {
     char curso[51];
     double nota01;
     double nota02;
-} registro;
+}
+registro;
 
 typedef struct {
     int size;
-    registro **matriz;
-} head;
+    registro ** matriz;
+}
+head;
 
-registro **Criar_matriz(int linha, int coluna) {
-    registro **matriz;
-    matriz = (registro **) calloc(sizeof(registro *), linha);
+registro ** Criar_matriz(int linha, int coluna) {
+    registro ** matriz;
+    matriz = (registro ** ) calloc(sizeof(registro * ), linha);
     if (matriz == NULL) {
         printf("Memoria insuficiente.\n");
         exit(1);
     }
     for (int i = 0; i < linha; i++) {
-        matriz[i] = (registro *) calloc(sizeof(registro), coluna);
+        matriz[i] = (registro * ) calloc(sizeof(registro), coluna);
         if (matriz[i] == NULL) {
             printf("Memoria insuficiente.\n");
             exit(1);
@@ -37,7 +43,7 @@ registro **Criar_matriz(int linha, int coluna) {
     return matriz;
 }
 
-int tamanho(char *string) {
+int tamanho(char * string) {
     int i = 0;
     while (string[i] != '\0') {
         i++;
@@ -45,10 +51,10 @@ int tamanho(char *string) {
     return i;
 }
 
-head Criar_Banco() {
-    head x;
-    x.matriz = Criar_matriz(MAXLinha, MAXColuna);
-    x.size = 0;
+head * Criar_Banco() {
+    head * x = (head * ) malloc(sizeof(head) * 1);
+    x -> matriz = Criar_matriz(MAXLinha, MAXColuna);
+    x -> size = 0;
     return x;
 }
 
@@ -61,34 +67,31 @@ registro Criar_registro() {
     while (1) {
         while (1) {
             printf("Insira o numero de matricula\n");
-            scanf("%d%*c", &matricula);
+            scanf("%d%*c", & matricula);
             if (matricula < 0) {
                 printf("matricula invalida insira um numero positivo\n");
                 continue;
-            }
-            else {
+            } else {
                 novo.matricula = matricula;
                 break;
             }
         }
         while (1) {
             printf("Insira a nota01\n");
-            scanf("%lf%*c", &nota01);
+            scanf("%lf%*c", & nota01);
             if (nota01 < 0 || nota01 > 10) {
                 printf("a nota deve estar em um intervalo de 0 a 10\n");
-            }
-            else {
+            } else {
                 novo.nota01 = nota01;
                 break;
             }
         }
         while (1) {
             printf("Insira a nota02\n");
-            scanf("%lf%*c", &nota02);
+            scanf("%lf%*c", & nota02);
             if (nota02 < 0 || nota02 > 10) {
                 printf("a nota deve estar em um intervalo de 0 a 10\n");
-            }
-            else {
+            } else {
                 novo.nota02 = nota02;
                 break;
             }
@@ -96,10 +99,9 @@ registro Criar_registro() {
         while (1) {
             printf("Insira o seu nome\n");
             scanf("%50[^\n]%*c", string);
-            if (tamanho(string)-1 < 0) {
+            if (tamanho(string) - 1 < 0) {
                 printf("O nome está vazio\n");
-            }
-            else {
+            } else {
                 strcpy(novo.nome, string);
                 break;
             }
@@ -107,10 +109,9 @@ registro Criar_registro() {
         while (1) {
             printf("Insira o nome do curso\n");
             scanf("%50[^\n]%*c", string);
-            if (tamanho(string)-1 < 0) {
+            if (tamanho(string) - 1 < 0) {
                 printf("O nome do curso está vazio\n");
-            }
-            else {
+            } else {
                 strcpy(novo.curso, string);
                 break;
             }
@@ -120,7 +121,7 @@ registro Criar_registro() {
     return novo;
 }
 
-bool Dado_Existente(int matricula, head *ref) {
+bool Dado_Existente(int matricula, head * ref) {
     for (int i = 0; i < MAXLinha; i++) {
         if (matricula == ref -> matriz[i][0].matricula) {
             return 1;
@@ -128,9 +129,9 @@ bool Dado_Existente(int matricula, head *ref) {
     }
 }
 
-int Inserir_registro (registro x, head *ref) {
-    if (!Dado_Existente(x.matricula, ref)){
-        ref -> matriz[ref->size][0] = x;
+int Inserir_registro(registro x, head * ref) {
+    if (!Dado_Existente(x.matricula, ref)) {
+        ref -> matriz[ref -> size][0] = x;
         ref -> size++;
     } else {
         printf("impossivel adicionar usuario presente no banco\n");
@@ -145,21 +146,24 @@ void Imprimir_Registro(registro x) {
     printf("- nota 02 = %lf                                           \n", x.nota02);
     printf("- Nome do Curso = %s                                      \n", x.curso);
     printf("- Nome do Aluno = %s                                      \n", x.nome);
-    printf("- Media do Aluno = %.2lf                                  \n", 
-        (x.nota01 + x.nota02)/2
-    );
+    printf("- Media do Aluno = %.2lf                                  \n",
+        (x.nota01 + x.nota02) / 2);
     printf("----------------------------------------------------------\n"),
 
-    printf("\n");
+        printf("\n");
 }
 
-void Imprimir_Dados (head *banco) {
-    for (int i = 0; i < banco -> size; i++) {
-       Imprimir_Registro(banco -> matriz[i][0]);
+void Imprimir_Dados(head * banco) {
+    if (banco != NULL) {
+        for (int i = 0; i < banco -> size; i++) {
+            Imprimir_Registro(banco -> matriz[i][0]);
+        }
+    } else {
+        printf("imposivel imprimir dados o banco não existe\n");
     }
 }
 
-registro Pegar_Registro(int matricula, head *ref) {
+registro Pegar_Registro(int matricula, head * ref) {
     registro x;
     for (int i = 0; i < ref -> size; i++) {
         if (matricula == ref -> matriz[i][0].matricula) {
@@ -169,11 +173,11 @@ registro Pegar_Registro(int matricula, head *ref) {
     return x;
 }
 
-void Liberar_Memoria(head *ref) {
-    registro *x;
+void Liberar_Memoria(head * ref) {
+    registro * x;
     if (ref != NULL) {
         for (int i = 0; i < ref -> size; i++) {
-            x = &(ref -> matriz[i][0]);
+            x = & (ref -> matriz[i][0]);
             free(x);
         }
 
@@ -183,8 +187,8 @@ void Liberar_Memoria(head *ref) {
     }
 }
 
-registro **deletar_registro(head *ref, int matricula) {
-    registro **Nova_Matriz = Criar_matriz(ref -> size-1, 1);
+registro ** deletar_registro(head * ref, int matricula) {
+    registro ** Nova_Matriz = Criar_matriz(ref -> size - 1, 1);
     int indice = 0;
     for (int i = 0; i < ref -> size; i++) {
         if (ref -> matriz[i][0].matricula != matricula) {
@@ -199,12 +203,25 @@ registro **deletar_registro(head *ref, int matricula) {
     return Nova_Matriz;
 };
 
+void Liberar_matriz(head ** ref) {
+    if ( * ref != NULL) {
+        head * aux = * ref;
+        for (int i = 0; i < aux -> size; i++) {
+            free(aux -> matriz[i]);
+        }
+        free(aux -> matriz);
+        free(aux);
+        * ref = NULL;
+    } else {
+        printf("matriz não existe impossivel deletar os dados presentes\n");
+    }
+}
 
-void Deletar_Dado (int matricula, head *ref) {
+void Deletar_Dado(int matricula, head * ref) {
     int opc = 0;
     if (Dado_Existente(matricula, ref)) {
         printf("Usuario existente deseja deletar (1 SIM) (2 NÃO) ?\n");
-        scanf("%d%*c", &opc);
+        scanf("%d%*c", & opc);
         if (opc == 1) {
             ref -> matriz = deletar_registro(ref, matricula);
         } else {
@@ -215,9 +232,9 @@ void Deletar_Dado (int matricula, head *ref) {
     }
 }
 
-int update(int matricula, head *ref, registro novo) {
+int update(int matricula, head * ref, registro novo) {
     for (int i = 0; i < ref -> size; i++) {
-        if(ref -> matriz[i][0].matricula == matricula) {
+        if (ref -> matriz[i][0].matricula == matricula) {
             ref -> matriz[i][0] = novo;
             return 1;
         }
@@ -226,27 +243,55 @@ int update(int matricula, head *ref, registro novo) {
 }
 
 int main() {
-    head banco = Criar_Banco();
-    for (int i = 0; i < 8; i++) {   
-        registro x = Criar_registro();
-        Inserir_registro(x, &banco);
+    head * list = Criar_Banco();
+    registro x;
+
+    for (int i = 0; i < 10; i++) {
+        x = Criar_registro();
+        Inserir_registro(x, list);
     }
-
-    registro b;
-    strcpy(b.curso, "matematica");
-    b.matricula = 8;
-    strcpy(b.nome, "matheus");
-    b.nota01 = 5;
-    b.nota02 = 9;
+    Inserir_registro(x, list);
+    Inserir_registro(x, list);
+    Inserir_registro(x, list);
 
 
-    Imprimir_Dados(&banco);
-    update(8, &banco, b);
-    Imprimir_Dados(&banco);
+    x = Criar_registro();
+    // no começo
+    update(2021010, list, x);
+    update(2021010, list, x);
 
-    printf("\n");
- 
+    // no meio
+    x = Criar_registro();
+    update(2021006, list, x);
+    update(2021006, list, x);
 
+    // no fim
+    x = Criar_registro();
+    update(2021001, list, x);
+    update(2021001, list, x);
+
+    Imprimir_Dados(list);
+
+    // deletar dados
+    // começo
+    Deletar_Dado(18, list);
+    // meio
+    Deletar_Dado(19, list);
+    // fim
+    Deletar_Dado(20, list);
+
+    Imprimir_Dados(list);
+
+    // destruindo
+    Liberar_matriz( & list);
+
+    // tentando imprimir a lista para verificar se foi deletada
+    Imprimir_Dados(list);
+
+    // FIM DO CRU
+    // CREATE Create_List() função responsavel pelo create
+    // REGISTER Add_Fist() função responsavel pelo registro
+    // UPDATE Update() função usado pelo update
 
     return 0;
 }
--- AED 2 (Algoritmos e estruta de dados 2)/Tarefas/01/02.c
+++ AED 2 (Algoritmos e estruta de dados 2)/Tarefas/01/02.c
@@ -1,119 +1,97 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <stdbool.h>
+
 #include <string.h>
 
-typedef struct
-{
+typedef struct {
     int matricula;
     char nome[51];
     char curso[51];
     double nota01;
     double nota02;
-} registro;
+}
+registro;
 
 typedef struct _node node;
 
-typedef struct
-{
-    node *begin;
-    node *end;
+typedef struct {
+    node * begin;
+    node * end;
     int size;
-} head;
+}
+head;
 
-struct _node
-{
+struct _node {
     registro value;
-    node *next;
-    node *prev;
+    node * next;
+    node * prev;
 };
 
-int tamanho(char *string)
-{
+int tamanho(char * string) {
     int i = 0;
-    while (string[i] != '\0')
-    {
+    while (string[i] != '\0') {
         i++;
     }
     return i;
 }
 
-registro Criar_registro()
-{
+registro Criar_registro() {
     registro novo;
     int matricula;
     double nota01, nota02;
     char string[51];
 
-    while (1)
-    {
-        while (1)
-        {
+    while (1) {
+        while (1) {
             printf("Insira o numero de matricula\n");
-            scanf("%d%*c", &matricula);
-            if (matricula < 0)
-            {
+            scanf("%d%*c", & matricula);
+            if (matricula < 0) {
                 printf("matricula invalida insira um numero positivo\n");
                 continue;
-            }
-            else
-            {
+            } else {
                 novo.matricula = matricula;
                 break;
             }
         }
-        while (1)
-        {
+        while (1) {
             printf("Insira a nota01\n");
-            scanf("%lf%*c", &nota01);
-            if (nota01 < 0 || nota01 > 10)
-            {
+            scanf("%lf%*c", & nota01);
+            if (nota01 < 0 || nota01 > 10) {
                 printf("a nota deve estar em um intervalo de 0 a 10\n");
-            }
-            else
-            {
+            } else {
                 novo.nota01 = nota01;
                 break;
             }
         }
-        while (1)
-        {
+        while (1) {
             printf("Insira a nota02\n");
-            scanf("%lf%*c", &nota02);
-            if (nota02 < 0 || nota02 > 10)
-            {
+            scanf("%lf%*c", & nota02);
+            if (nota02 < 0 || nota02 > 10) {
                 printf("a nota deve estar em um intervalo de 0 a 10\n");
-            }
-            else
-            {
+            } else {
                 novo.nota02 = nota02;
                 break;
             }
         }
-        while (1)
-        {
+        while (1) {
             printf("Insira o seu nome\n");
             scanf("%50[^\n]%*c", string);
-            if (tamanho(string) - 1 < 0)
-            {
+            if (tamanho(string) - 1 < 0) {
                 printf("O nome está vazio\n");
-            }
-            else
-            {
+            } else {
                 strcpy(novo.nome, string);
                 break;
             }
         }
-        while (1)
-        {
+        while (1) {
             printf("Insira o nome do curso\n");
             scanf("%50[^\n]%*c", string);
-            if (tamanho(string) - 1 < 0)
-            {
+            if (tamanho(string) - 1 < 0) {
                 printf("O nome do curso está vazio\n");
-            }
-            else
-            {
+            } else {
                 strcpy(novo.curso, string);
                 break;
             }
@@ -123,198 +101,233 @@ registro Criar_registro()
     return novo;
 }
 
-head *Create_List()
-{
-    head *x = NULL;
-    do
-    {
-        x = (head *)malloc(sizeof(head) * 1);
-        x->begin = NULL;
-        x->end = NULL;
-        x->size = 0;
+head * Create_List() {
+    head * x = NULL;
+    do {
+        x = (head * ) malloc(sizeof(head) * 1);
+        x -> begin = NULL;
+        x -> end = NULL;
+        x -> size = 0;
         return x;
     } while (x == NULL);
 };
 
-node *Create_Node(registro x)
-{
-    node *aux = NULL;
-    do
-    {
-        aux = (node *)malloc(sizeof(node) * 1);
+node * Create_Node(registro x) {
+    node * aux = NULL;
+    do {
+        aux = (node * ) malloc(sizeof(node) * 1);
     } while (aux == NULL);
-    aux->next = NULL;
-    aux->prev = NULL;
-    aux->value = x;
+    aux -> next = NULL;
+    aux -> prev = NULL;
+    aux -> value = x;
     return aux;
 };
 
-void Add_Fist(registro x, head *list)
-{
-    if (list != NULL)
-    {
-        node *aux = Create_Node(x);
-        node *ref = NULL;
-
-        if (list->size == 0)
-        {
-            list->begin = aux;
-            list->end = aux;
-        }
-        else
-        {
-            ref = list->begin;
-            list->begin = aux;
-            list->begin->next = ref;
-            ref->prev = list->begin;
-        }
-        list->size++;
-
-        // return true;
-    }
-    else
-    {
-        printf("Memmory not alocated\n");
-        // return false;
-    }
-}
-
-bool imprimir(head *x)
-{
-    node *aux = NULL;
-    aux = x->begin;
-    for (int i = 0; i < x->size; i++)
-    {
-        printf("\n");
-        printf("----------------------------------------------------------\n");
-        printf("- matricula = %d                                          \n", aux->value.matricula);
-        printf("- nota 01 = %lf                                           \n", aux->value.nota01);
-        printf("- nota 02 = %lf                                           \n", aux->value.nota02);
-        printf("- Nome do Curso = %s                                      \n", aux->value.nome);
-        printf("- Nome do Aluno = %s                                      \n", aux->value.curso);
-        printf("- Media do Aluno = %.2lf                                  \n",
-               (aux->value.nota01 + aux->value.nota02) / 2);
-        printf("----------------------------------------------------------\n"),
-
-        aux = aux->next;
-    }
-}
-
-bool IsPresent(int matricula, head *list)
-{
-    node *aux = list->begin;
-    for (int i = 0; i < list->size; i++)
-    {
-        if (aux->value.matricula == matricula)
-        {
-            return true;
-        }
-        aux = aux->next;
-    }
-    return false;
-}
-
-void Destruct_node(node **ref) {
-    if (ref != NULL && *ref != NULL) {
-        node *aux = *ref;
-        free(aux);  
-        *ref = NULL;
-    }
-}
-
-void Update(int matricula, registro new, head *list)
-{
-    node *aux = Create_Node(new);
-    node *cm = list -> begin;
-
-    if (IsPresent(matricula, list))
-    {
-        for (int i = 0; i < list -> size; i++)
-        {
-            if (cm->value.matricula == matricula)
-            {
-                if (cm == list->end)
-                {
-                    list -> end -> next = aux;
-                    aux -> prev = list -> end;
-                    list -> end = aux;
-                }
-                if (cm == list->begin)
-                {
-                    aux -> next = list -> begin -> next;
-                    list -> begin -> next -> prev = aux;
-                    list -> begin = aux;
-                }
-                else
-                {
-                    aux -> prev = cm -> prev;
-                    aux -> next = cm -> next;
-                    cm -> prev -> next = aux;
-                    cm -> next -> prev = aux;
-                }
+bool IsPresent(int matricula, head * list) {
+    if (list != NULL) {
+        node * aux = list -> begin;
+        for (int i = 0; i < list -> size; i++) {
+            if (aux -> value.matricula == matricula) {
+                return true;
             }
-            cm = cm -> next;
+            aux = aux -> next;
         }
-        Destruct_node(&cm);
-    }
-    else
-    {
-        printf("O Elemento está presente no array\n");
+        return false;
+    } else {
+        printf("A lista não existe impossivel verificar a presença de um cadastro\n");
+        exit(0);
     }
 }
 
-void Delete(head *list, int matricula) {
-    if (IsPresent(matricula, list)) {
-        int op = 0;
-        printf("Usuario existente deseja deletar (1 SIM) (2 NÃO) ?\n");
-        scanf("%d%*c", &op);
-        if(op == 1) {
-            node *aux = list -> begin;
-            for (int i = 0; i < list -> size; i++) {
-                if (aux -> value.matricula ==  matricula) {
-                    if (aux == list -> end) {
-                        list -> end = list -> end -> prev;
-                        break;
-                    }
-                    else if (aux == list -> begin) {
-                        list -> begin = list -> begin -> next;
-                        break;
-                    } else {
-                        aux -> prev -> next = aux -> next;
-                        aux -> next -> prev = aux -> prev;
-                    }
-                }
-                aux = aux -> next;
+void Add_Fist(registro x, head * list) {
+    if (list != NULL) {
+        if (!IsPresent(x.matricula, list)){
+            node * aux = Create_Node(x);
+            node * ref = NULL;
+
+            if (list -> size == 0) {
+                list -> begin = aux;
+                list -> end = aux;
+            } else {
+                ref = list -> begin;
+                list -> begin = aux;
+                list -> begin -> next = ref;
+                ref -> prev = list -> begin;
             }
-            list -> size--;
+            list -> size++;
         } else {
-            printf("Nenhum elemento foi deletado do array\n");
+            printf("elemento duplicado nada foi adicionado\n");
         }
-    } else  {
-        printf("O valor não está presente no array\n");
+        
+    } else {
+        printf("A lista não existe impossivel adicionar o elemento\n");
+        exit(0);
     }
 }
 
-int main()
-{
-    head *list = Create_List();
-    registro x = Criar_registro();
-    /*
-    for (int i = 0; i < 5; i++)
-    {
-        registro x = Criar_registro();
+bool imprimir(head * x) {
+    if (x != NULL) {
+        node * aux = NULL;
+        aux = x -> begin;
+        for (int i = 0; i < x -> size; i++) {
+            printf("\n");
+            printf("----------------------------------------------------------\n");
+            printf("- matricula = %d                                          \n", aux -> value.matricula);
+            printf("- nota 01 = %lf                                           \n", aux -> value.nota01);
+            printf("- nota 02 = %lf                                           \n", aux -> value.nota02);
+            printf("- Nome do Curso = %s                                      \n", aux -> value.nome);
+            printf("- Nome do Aluno = %s                                      \n", aux -> value.curso);
+            printf("- Media do Aluno = %.2lf                                  \n",
+                (aux -> value.nota01 + aux -> value.nota02) / 2);
+            printf("----------------------------------------------------------\n"),
+
+                aux = aux -> next;
+        }
+    } else {
+        printf("A lista não existe impossivel exibir algo\n");
+        exit(0);
+    }
+}
+
+void Destruct_node(node ** ref) {
+    if (ref != NULL && * ref != NULL) {
+        node * aux = * ref;
+        free(aux);
+        * ref = NULL;
+    }
+}
+
+void Destruct_list(head ** ref) {
+    if ( * ref != NULL) {
+        head * H_aux = * ref;
+        node * ptr = ( * ref) -> begin;
+        node * aux = NULL;
+
+        while (ptr -> next != NULL) {
+            aux = ptr;
+            ptr = ptr -> next;
+            Destruct_node( & aux);
+        }
+
+        free(H_aux);
+        * ref = NULL;
+    } else {
+        printf("Lista vazia não há nada para deletar programa finalizado\n");
+        exit(0);
+    }
+}
+
+void Update(int matricula, registro new, head * list) {
+    if (list != NULL) {
+        node * cm = list -> begin;
+
+        if (IsPresent(matricula, list)) {
+            for (int i = 0; i < list -> size; i++) {
+                if (cm -> value.matricula == matricula) {
+                    if (cm == list -> end) {
+                        list -> end -> value = new;
+                    }
+                    if (cm == list -> begin) {
+                        list -> begin -> value = new;
+                    } else {
+                        cm -> value = new;
+                    }
+                }
+                cm = cm -> next;
+            }
+            Destruct_node( & cm);
+        } else {
+            printf("O Elemento não está presente no array\n");
+        }
+    } else {
+        printf("A lista de registro não existe programa finalizado\n");
+    }
+}
+
+void Delete(head * list, int matricula) {
+    if (list != NULL) {
+        if (IsPresent(matricula, list)) {
+            int op = 0;
+            printf("Usuario existente deseja deletar (1 SIM) (2 NÃO) ?\n");
+            scanf("%d%*c", & op);
+            if (op == 1) {
+                node * aux = list -> begin;
+                for (int i = 0; i < list -> size; i++) {
+                    if (aux -> value.matricula == matricula) {
+                        if (aux == list -> end) {
+                            list -> end = list -> end -> prev;
+                            break;
+                        } else if (aux == list -> begin) {
+                            list -> begin = list -> begin -> next;
+                            break;
+                        } else {
+                            aux -> prev -> next = aux -> next;
+                            aux -> next -> prev = aux -> prev;
+                        }
+                    }
+                    aux = aux -> next;
+                }
+                list -> size--;
+            } else {
+                printf("Nenhum elemento foi deletado do array\n");
+            }
+        } else {
+            printf("O valor não está presente no array\n");
+        }
+    } else {
+        printf("Lista de registros não existe programa finalizado\n");
+        exit(0);
+    }
+}
+
+int main() {
+    head * list = Create_List();
+    registro x;
+
+    for (int i = 0; i < 10; i++) {
+        x = Criar_registro();
         Add_Fist(x, list);
     }
 
-    registro teste = Criar_registro();
-    
-    
-    Update(3, teste, list);
-    Delete(list, 1);
-    Delete(list, 111);
-    */
-    /// printf("%d", list -> begin->value.matricula);
+    //tentando adicionar duplicado
+    Add_Fist(x, list);
+    Add_Fist(x, list);
+    Add_Fist(x, list);
+
+
+    x = Criar_registro();
+    // no começo
+    Update(2021010, x, list);
+
+    // no meio
+    x = Criar_registro();
+    Update(2021006, x, list);
+
+  
+    // no fim
+    x = Criar_registro();
+    Update(2021001, x, list);
+
+    // deletar dados
+    // começo
+    Delete(list, 18);
+    // meio
+    Delete(list, 19);
+    // fim
+    Delete(list, 20);
+
     imprimir(list);
+
+    //destruindo
+    Destruct_list( & list);
+
+    //tentando imprimir a lista para verificar se foi deletada
+    imprimir(list);
+
+    //FIM DO CRU 
+    //CREATE Create_List() função responsavel pelo create
+    //REGISTER Add_Fist() função responsavel pelo registro
+    //UPDATE Update() função usada no update
 
     return 0;
 }
